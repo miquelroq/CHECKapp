df51d9c9304ffa7842b539538197a28430561fff
Tiago <tiagomelo@ua.pt>
2020-08-22 14:04:27 +0100

    Very raw version of sliders, fragments and checkup process - needs content, proper buttons and stuff

--- android/app/src/main/java/com/example/alarm_doc/CheckupProcess.java
+++ android/app/src/main/java/com/example/alarm_doc/CheckupProcess.java
@@ -1,57 +1,184 @@
 package com.example.alarm_doc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.google.android.material.tabs.TabLayout;
 
-public class CheckupProcess extends AppCompatActivity implements Question1.OnFragmentInteractionListener,
-                                                                    Question2.OnFragmentInteractionListener,
-                                                                    Question3.OnFragmentInteractionListener {
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_checkup_process);
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
 
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+public class CheckupProcess extends Activity {
 
-        final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
-        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        private ViewPager viewPager;
+        private MyViewPagerAdapter myViewPagerAdapter;
+        private LinearLayout dotsLayout;
+        private ImageView[] dots;
+        private int[] layouts;
+        private Button btnBack, btnNext;
+        private int dotscount;
+        Button btnToApp;
+        Button skipBtn;
 
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_checkup_process);
+            viewPager =  findViewById(R.id.view_pager);
+            dotsLayout = findViewById(R.id.layoutDots);
+            btnBack = findViewById(R.id.btn_back);
+            btnNext =  findViewById(R.id.btn_next);
+            skipBtn = findViewById(R.id.skipBtn);
+
+            // layouts of welcome sliders
+            layouts = new int[]{
+                    R.layout.fragment_question1,
+                    R.layout.fragment_question2,
+                    R.layout.fragment_question3
+            };
+
+            // adding bottom dots
+            dotscount =3;
+            dots = new ImageView[dotscount];
+            for(int i = 0; i < dotscount; i++) {
+                dots[i] = new ImageView(this);
+                dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.no_active_dot));
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                params.setMargins(8, 0, 8, 0);
+                dotsLayout.addView(dots[i], params);
+
+            }
+            dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+            myViewPagerAdapter = new MyViewPagerAdapter();
+            viewPager.setAdapter(myViewPagerAdapter);
+            viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+            btnBack.setBackgroundResource(R.drawable.alarm_doc);
+            btnBack.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int current = getItem(-1);
+                    if (current < layouts.length) {
+                        viewPager.setCurrentItem(current);
+                    }
+                }
+            });
+
+            btnNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int current = getItem(+1);
+                    if (current < layouts.length) {
+                        viewPager.setCurrentItem(current);
+                    }
+                }
+            });
+
+            skipBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(CheckupProcess.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+
+
+        private int getItem(int i) {
+            return viewPager.getCurrentItem() + i;
+        }
+
+        ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
+            public void onPageSelected(int position) {
+                for(int i = 0; i< dotscount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.no_active_dot));
+                }
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+                if (position == layouts.length - 1) {
+                    btnNext.setBackgroundResource(R.drawable.alarm_doc);
+                }
+                else if (position == 0){
+                    btnBack.setBackgroundResource(R.drawable.alarm_doc);
+                }
+                else{
+                    btnBack.setBackgroundResource(R.drawable.alarm_doc);
+                    btnNext.setBackgroundResource(R.drawable.alarm_doc);
+                }
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
 
             }
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {
+            public void onPageScrollStateChanged(int arg0) {
 
             }
-        });
+        };
+
+        public class MyViewPagerAdapter extends PagerAdapter {
+            private LayoutInflater layoutInflater;
+
+            public MyViewPagerAdapter() {
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                View view = layoutInflater.inflate(layouts[position], container, false);
+                if (position==2) {
+
+                }
+                container.addView(view);
+
+                return view;
+            }
+
+            @Override
+            public int getCount() {
+                return layouts.length;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object obj) {
+                return view == obj;
+            }
+
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                View view = (View) object;
+                container.removeView(view);
+            }
+        }
 
     }
-
-    @Override
-    public void onFragmentInteraction(Uri uri){
-        // EMPTY
-    }
-
-}
